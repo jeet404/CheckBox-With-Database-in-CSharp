bd45e7c35b2672ea648dee565fe21c1ac4288b5b
jeet404 <jeet63240@gmail.com>
2022-03-27 11:11:43 +0530

    Add Files.

---
+++ CheckBoxWithDataBaseWinApp/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CheckBoxWithDataBaseWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chk2 = new System.Windows.Forms.CheckBox();
+            this.chk1 = new System.Windows.Forms.CheckBox();
+            this.chk4 = new System.Windows.Forms.CheckBox();
+            this.chk5 = new System.Windows.Forms.CheckBox();
+            this.chk3 = new System.Windows.Forms.CheckBox();
+            this.chk6 = new System.Windows.Forms.CheckBox();
+            this.lbl_stat = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.txt_sid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chk2
+            // 
+            this.chk2.AutoSize = true;
+            this.chk2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk2.Location = new System.Drawing.Point(12, 170);
+            this.chk2.Name = "chk2";
+            this.chk2.Size = new System.Drawing.Size(83, 24);
+            this.chk2.TabIndex = 0;
+            this.chk2.Text = "Python";
+            this.chk2.UseVisualStyleBackColor = true;
+            // 
+            // chk1
+            // 
+            this.chk1.AutoSize = true;
+            this.chk1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk1.Location = new System.Drawing.Point(12, 140);
+            this.chk1.Name = "chk1";
+            this.chk1.Size = new System.Drawing.Size(112, 24);
+            this.chk1.TabIndex = 1;
+            this.chk1.Text = "JavaScript";
+            this.chk1.UseVisualStyleBackColor = true;
+            // 
+            // chk4
+            // 
+            this.chk4.AutoSize = true;
+            this.chk4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk4.Location = new System.Drawing.Point(162, 140);
+            this.chk4.Name = "chk4";
+            this.chk4.Size = new System.Drawing.Size(63, 24);
+            this.chk4.TabIndex = 2;
+            this.chk4.Text = "PHP";
+            this.chk4.UseVisualStyleBackColor = true;
+            // 
+            // chk5
+            // 
+            this.chk5.AutoSize = true;
+            this.chk5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk5.Location = new System.Drawing.Point(162, 170);
+            this.chk5.Name = "chk5";
+            this.chk5.Size = new System.Drawing.Size(73, 24);
+            this.chk5.TabIndex = 3;
+            this.chk5.Text = "JAVA";
+            this.chk5.UseVisualStyleBackColor = true;
+            // 
+            // chk3
+            // 
+            this.chk3.AutoSize = true;
+            this.chk3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk3.Location = new System.Drawing.Point(12, 200);
+            this.chk3.Name = "chk3";
+            this.chk3.Size = new System.Drawing.Size(50, 24);
+            this.chk3.TabIndex = 4;
+            this.chk3.Text = "C#";
+            this.chk3.UseVisualStyleBackColor = true;
+            // 
+            // chk6
+            // 
+            this.chk6.AutoSize = true;
+            this.chk6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk6.Location = new System.Drawing.Point(162, 200);
+            this.chk6.Name = "chk6";
+            this.chk6.Size = new System.Drawing.Size(60, 24);
+            this.chk6.TabIndex = 5;
+            this.chk6.Text = "C++";
+            this.chk6.UseVisualStyleBackColor = true;
+            // 
+            // lbl_stat
+            // 
+            this.lbl_stat.AutoSize = true;
+            this.lbl_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_stat.Location = new System.Drawing.Point(176, 314);
+            this.lbl_stat.Name = "lbl_stat";
+            this.lbl_stat.Size = new System.Drawing.Size(110, 24);
+            this.lbl_stat.TabIndex = 6;
+            this.lbl_stat.Text = "                    ";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(167, 241);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(112, 36);
+            this.btn_add.TabIndex = 7;
+            this.btn_add.Text = "Add Skills";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_show.Location = new System.Drawing.Point(167, 65);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(112, 36);
+            this.btn_show.TabIndex = 8;
+            this.btn_show.Text = "Show Skills";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // txt_sid
+            // 
+            this.txt_sid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sid.Location = new System.Drawing.Point(12, 70);
+            this.txt_sid.Name = "txt_sid";
+            this.txt_sid.Size = new System.Drawing.Size(149, 26);
+            this.txt_sid.TabIndex = 9;
+            this.txt_sid.TextChanged += new System.EventHandler(this.txt_sid_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Enter Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Connection Satus :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(291, 347);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_sid);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.lbl_stat);
+            this.Controls.Add(this.chk6);
+            this.Controls.Add(this.chk3);
+            this.Controls.Add(this.chk5);
+            this.Controls.Add(this.chk4);
+            this.Controls.Add(this.chk1);
+            this.Controls.Add(this.chk2);
+            this.Name = "Form1";
+            this.Text = "CheckBox With DataBase";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chk2;
+        private System.Windows.Forms.CheckBox chk1;
+        private System.Windows.Forms.CheckBox chk4;
+        private System.Windows.Forms.CheckBox chk5;
+        private System.Windows.Forms.CheckBox chk3;
+        private System.Windows.Forms.CheckBox chk6;
+        private System.Windows.Forms.Label lbl_stat;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.TextBox txt_sid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CheckBoxWithDataBaseWinApp/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CheckBoxWithDataBaseWinApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+        string sk;
+        CheckBox[] itm = new CheckBox[6];
+        public Form1()
+        {
+            InitializeComponent();
+            itm[0] = chk1;
+            itm[1] = chk2;
+            itm[2] = chk3;
+            itm[3] = chk4;
+            itm[4] = chk5;
+            itm[5] = chk6;
+        }
+
+        private void skillin()
+        {
+            sk = "";
+            foreach (CheckBox c in itm)
+            {
+                if (c.Checked)
+                {
+                    sk += c.Text + ",";
+                }
+            }
+            sk = sk.TrimEnd(',');
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\CheckBoxWithDataBaseWinApp\CheckBoxWithDataBaseWinApp\SkillsDB.mdf';Integrated Security=True");
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    lbl_stat.BackColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                lbl_stat.BackColor = Color.Red;
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            skillin();
+            cmd = new SqlCommand("INSERT INTO skill_details VALUES ('"+sk+"')", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Skill Inserted..");
+            foreach (CheckBox c in itm)
+            {
+                c.Checked = false;
+            }
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("SELECT * FROM skill_details WHERE Id = "+txt_sid.Text+"",conn);
+            dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                string[] sk_disp = dt.Rows[0][1].ToString().Split(',');
+                for (int i = 0; i < sk_disp.Length; i++)
+                {
+                    foreach (CheckBox c in itm)
+                    {
+                        if (c.Text == sk_disp[i])
+                        {
+                            c.Checked = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Data Found!");
+            }
+        }
+
+        private void txt_sid_TextChanged(object sender, EventArgs e)
+        {
+            foreach(CheckBox c in itm)
+            {
+                c.Checked = false;
+            }
+        }
+    }
+}
